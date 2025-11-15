909b8b4e92edf8d964da79d04d01364b07dfad95
Aayush-wellness <aayushwellnessltd@gmail.com>
2025-11-15 12:01:01 +0530

    Update ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -33,9 +33,7 @@ const ContactForm = () => {
           <h2>Contact Info</h2>
           <img src="https://cdn.shopify.com/s/files/1/0636/5226/6115/files/pexels-brittany-17733-87812.jpg?v=1757337749" alt="Support" className="info-img" />
 
-          <p><strong>Phone:</strong> 123-456-7890</p>
-          <p><strong>Email:</strong> hello@healthcare.com</p>
-          <p><strong>Location:</strong> 123 Anywhere St, Any City, 12345</p>
+   
         </div>
       </div>
     </section>
